1f776df7f1b832baa86a3605e4584e2d52136ec2
hanlin-ren <e1999@foxmail.com>
2020-09-29 19:49:12 +0800

    Create block.js

---
+++ block.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         block list
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  nmsl
+// @author       Xiaochuan Sun
+// @include      https://www.zhihu.com/
+// @include      https://www.zhihu.com/explore
+// @include      https://daily.zhihu.com/
+// @include      https://www.zhihu.com/roundtable
+// @include      https://www.zhihu.com/question/waiting
+// @include      https://twitter.com/explore
+// @include      https://twitter.com/explore/tabs/for-you
+// @include      https://www.quora.com/
+// @include      https://www.quora.com/spaces
+// @include      https://github.com/explore
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert("你看你妈呢？");
+    document.body.innerHTML = "";
+})();
