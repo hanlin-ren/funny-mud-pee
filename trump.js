df4d1116412ed553277778d630b8e80e9bbf0964
hanlin-ren <e1999@foxmail.com>
2020-10-07 11:59:52 +0800

    Create trump.js

---
+++ trump.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         long live president trump
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  nmsl
+// @author       Xiaochuan Sun
+// @include      https://twitter.com/realDonaldTrump
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.body.innerHTML = "";
+    alert("在？为什么要看尸体的推特？");
+})();
